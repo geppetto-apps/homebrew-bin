62b2841407bfffb5e961a58d3f4cb84f92cda550
Theodor Tonum <theodor@tonum.no>
2016-08-27 15:35:35 +0200

    Update crow formula to v0.3.0

--- crow.rb
+++ crow.rb
@@ -1,16 +1,14 @@
 class Crow < Formula
   desc "Transpile/compile Crystal to Flow"
   homepage "http://crow.geppetto.no/index.html"
-  url "https://github.com/geppetto-apps/crow/archive/v0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "e4585a01ca27c6a2f2d5d576f8f3186d98dc9a6b4cb269dab43388765853475e"
+  url "https://github.com/geppetto-apps/crow/archive/v0.3.0.tar.gz"
+  version "0.3.0"
+  sha256 "112b1111b25f122c41919a4ff164fe8c315eb184c40c6f53caa3c8bb02d875d4"
   head "https://github.com/geppetto-apps/crow.git"
 
   depends_on "crystal-lang" => :build
 
   def install
-    system "mkdir -p build"
-    system "crystal deps"
     system "bin/build"
     bin.install "build/crow"
   end
